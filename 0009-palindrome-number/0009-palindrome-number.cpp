46031b295a393df5a275d03afc59f57a8c5e1108
Aniket Maurya <aniket24112007@gmail.com>
2026-08-29 18:49:54 +0530

    Time: 0 ms (100%), Space: 8.6 MB (35.87%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int n) {
+        long original = n;
+        long reverse = 0;
+
+        while(n>0){
+            int digit = n%10;
+            reverse = reverse *10 + digit;
+            n=n/10;
+        }
+        if(original == reverse) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
